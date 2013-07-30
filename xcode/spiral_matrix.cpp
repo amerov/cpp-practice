05fc7f7f14eb3172303f6ecaaacef724d6a3fe2a
Denny C. Dai <dennycd@me.com>
2013-07-30 01:07:02 -0700

    spinal matrix

---
+++ xcode/spiral_matrix.cpp
@@ -0,0 +1,17 @@
+/**
+REF http://discuss.leetcode.com/questions/236/spiral-matrix-ii
+ 
+ Given an integer n, generate a square matrix filled with elements from 1 to n^2 in spiral order.
+ 
+ For example,
+ Given n = 3,
+ 
+ You should return the following matrix:
+ [
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+ ]
+ 
+**/
+
