e84d1b4a667cd9d07d7f891247320b846a739f9e
Denny C. Dai <dennycd@me.com>
2013-08-02 21:50:29 -0700

    path sum implementation

--- test/path_sum_II.cpp
+++ test/path_sum_II.cpp
@@ -23,11 +23,41 @@
  **/
 
 #include <iostream>
+#include <vector>
 #include <stack>
 #include <cassert>
 using namespace std;
 
+//tree node
+struct Node{
+    Node*   lchild;
+    Node*   rchild;
+    int     value;
+};
 
+//return paths
+typedef std::vector<std::vector<int>> Paths;
+
+
+
+
+/**
+  given a tree, find all paths whose value sum equals to the given sum
+ **/
+void get_sum(const int& targetSum,  int curSum, const Node* node, Paths& paths){
+    if(!node) return;
+    
+    curSum = targetSum + node->value;
+    if(curSum == targetSum){
+        //enter [root ... cur node] into path
+        vector<int> path;
+        
+    }
+    
+    //value could be 0 or negative, so should further check 
+    get_sum(targetSum, curSum, node->lchild, paths);
+    get_sum(targetSum, curSum, node->rchild, paths);
+}
 
 
 int main(int argc, char** argv)
