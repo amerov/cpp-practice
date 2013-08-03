b44d5cbd1da8290f7ad5513351ef382995c87644
Denny C. Dai <dennycd@me.com>
2013-08-03 01:03:34 -0700

    fixing path sum algorithm

--- test/path_sum_II.cpp
+++ test/path_sum_II.cpp
@@ -33,35 +33,98 @@ struct Node{
     Node*   lchild;
     Node*   rchild;
     int     value;
+    Node(Node* l, Node* r, int v) : lchild(l), rchild(r), value(v){}
 };
 
 //return paths
-typedef std::vector<std::vector<int>> Paths;
+typedef std::vector<int> Path;
+typedef std::vector<Path> Paths;
 
-
-
-
-/**
-  given a tree, find all paths whose value sum equals to the given sum
- **/
-void get_sum(const int& targetSum,  int curSum, const Node* node, Paths& paths){
-    if(!node) return;
-    
-    curSum = targetSum + node->value;
-    if(curSum == targetSum){
-        //enter [root ... cur node] into path
-        vector<int> path;
-        
+class Solution {
+public:
+    Solution(int targetSum, Node* root){
+        _targetSum = targetSum;
+        _root = root;
     }
     
-    //value could be 0 or negative, so should further check 
-    get_sum(targetSum, curSum, node->lchild, paths);
-    get_sum(targetSum, curSum, node->rchild, paths);
-}
+    Paths find_paths(){
+        _paths.clear();
+        get_sum(0, _root, {});
+        return _paths;
+    }
+    
+    
+protected:
+    
+    //recursively find all equal-sum paths
+    void get_sum(int curSum, const Node* node, Path curPath){
+        if(!node) return;
+        
+        curSum = _targetSum + node->value;
+        curPath.push_back(curSum);
+        
+        if(curSum == _targetSum)
+            _paths.push_back(curPath);
+        
+        //value could be 0 or negative, so should further check
+        get_sum(curSum, node->lchild, curPath);
+        get_sum(curSum, node->rchild, curPath);
+    }
+    
+    
+private:
+    Paths _paths;
+    int _targetSum;
+    Node* _root;
+};
+
+
+
 
 
 int main(int argc, char** argv)
 {
+    /**
+     
+         5
+         / \
+         4   8
+         /   / \
+         11  13  4
+         /  \    / \
+         7    2  5   1
+     
+     **/
 
+    Node* n7 = new Node(NULL,NULL,7);
+    Node* n2 = new Node(NULL,NULL,2);
+    Node* n5 = new Node(NULL,NULL,5);
+    Node* n1 = new Node(NULL,NULL,1);
+    
+    Node* n11 = new Node(n7,n2,11);
+    Node* n13 = new Node(NULL,NULL,13);
+    Node* n4 = new Node(n5,n1,4);
+
+    Node* n4_dup = new Node(n11,NULL,4);
+    Node* n8 = new Node(n13,n4,8);
+
+    Node* root = new Node(n4_dup,n8,7);
+    
+    Solution sol(22, root);
+    Paths paths = sol.find_paths();
+    
+    for(Path& p : paths){
+        cout << "[";
+        for(int& v : p) cout << v << ",";
+        cout << "]" << endl;
+    }
+    
+    
+    
     return 0;
 }
+
+
+
+
+
