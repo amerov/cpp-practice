9c1914e53431efc2a188d088771b7fe730318350
Denny C. Dai <dennycd@me.com>
2013-08-04 23:54:54 -0700

    symmetric tree checker

--- xcode/symmetric_tree.cpp
+++ xcode/symmetric_tree.cpp
@@ -21,3 +21,93 @@
  Bonus points if you could solve it both recursively and iteratively.
  
 **/
+
+#include <iostream>
+#include <cassert>
+#include <queue>
+#include <stack>
+
+/**
+  Given a tree, to check whether it is symmetric
+ 
+  - in-order traversal, the result string is symmetric (reverse is identical)
+ **/
+
+using namespace std;
+
+struct Node {
+    Node*   lchild;
+    Node*   rchild;
+    int     key;
+    Node(Node* l, Node* r, int k) : lchild(l), rchild(r), key(k){}
+};
+
+
+class Solution{
+public:
+    Solution(Node* root){
+        _root = root;
+    }
+    
+    bool symmetric(){
+        queue<int> q;
+        stack<int> stk;
+        _inorder_traverse(_root, q, stk);
+        
+        while (!stk.empty()) {
+            if(stk.top()!=q.front()) return false;
+            stk.pop();
+            q.pop();
+        }
+        
+        return true;
+    };
+    
+protected:
+    void _inorder_traverse(Node* node, queue<int>& queue, stack<int> & stk){
+        if(!node) return;
+        _inorder_traverse(node->lchild, queue, stk);
+        queue.push(node->key);
+        stk.push(node->key);
+        _inorder_traverse(node->rchild, queue, stk);
+    }
+    
+private:
+    Node* _root;
+};
+
+
+
+int main(int argc, char *argv[]){
+    
+    /**
+     
+     1
+     / \
+     2   2
+     / \ / \
+     3  4 4  3
+     
+     **/
+    
+    Node* n3 = new Node(NULL,NULL,3);
+    Node* n4 = new Node(NULL,NULL,4);
+    Node* n2 = new Node(n3,n4,2);
+    
+    
+    Node* m3 = new Node(NULL,NULL,3);
+    Node* m4 = new Node(NULL,NULL,4);
+    Node* m2 = new Node(m4,m3,2);
+    
+    Node* root = new Node(n2,m2,1);
+    
+    Solution sol(root);
+    
+    cout << (sol.symmetric() ? "symmetric" : "not symmetric") << endl;
+    assert(sol.symmetric());
+    
+    
+    return 0;
+}
+
+
