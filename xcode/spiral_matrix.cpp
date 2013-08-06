e55d2421957a51d7669ee452d3b38ced6ee175a5
Denny C. Dai <dennycd@me.com>
2013-08-05 23:44:38 -0700

    spiral matrix problem

--- xcode/spiral_matrix.cpp
+++ xcode/spiral_matrix.cpp
@@ -15,3 +15,60 @@ REF http://discuss.leetcode.com/questions/236/spiral-matrix-ii
  
 **/
 
+//struct Position{
+//    int i;
+//    int j;
+//};
+
+
+class Solution {
+public:
+    Solution(int n){
+        _n = n;
+        _matrix = new int[n*n];
+        for(int i=0;i<n*n;i++)
+            _matrix[i] = 0;
+    }
+    
+    ~Solution(){
+        delete[] _matrix;
+    }
+    
+    
+    
+    const int* get_matrix(){
+
+        return _matrix;
+    };
+    
+    
+protected:
+    
+    /**
+      given current pos, infer the next index to move into
+     **/
+    int next_move(int cur){
+        int i = cur / _n;
+        int j = cur % _n;
+        
+        //move rightwards
+        if(i-1<0 || _matrix[i-1]>0)  //north invalid
+            if(j+1 < _n && _matrix[j+1]==0) //east valid
+                return i*_n+j+1;
+            else{
+                if(i+1 < _n && _matrix[])
+            }
+        
+        
+        
+    }
+    
+private:
+    int _n;
+    int* _matrix;
+};
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
