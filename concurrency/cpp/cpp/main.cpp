44395539fdda1c905d9fa0a2313ac54b5aea1b93
Denny C. Dai <dennycd@me.com>
2013-09-02 22:43:35 -0700

    initial import of C++11 concurrency test project

---
+++ concurrency/cpp/cpp/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by Denny C. Dai on 9/2/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#include <iostream>
+
+/**
+  Concurrency C++11 
+ **/
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
