9f2e210fca8be6d444ec6f4284d41f5d95f20bdc
Denny C. Dai <dennycd@me.com>
2013-09-04 23:49:50 -0700

    adding language basic test project

---
+++ lang_basic/LangTest/LangTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LangTest
+//
+//  Created by Denny C. Dai on 9/4/2013.
+//  Copyright (c) 2013 Denny C. Dai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
